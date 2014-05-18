9c69b6e2ee61d52d9b9fea0571a8eae4f53c37c4
WenJun <wenjun1055@gmail.com>
2014-05-18 23:48:36 +0800

    能初步通信

--- server.c
+++ server.c
@@ -21,17 +21,13 @@ static int parse_options(int argc, char **argv)
 {
     int opt;
     struct option longopts[] = {
-        {"is_debug",    0,  NULL,   'D'},
         {"port",        1,  NULL,   'p'},
         {"is_log",      0,  NULL,   'l'},
         {NULL,          0,  NULL,     0}
     };
     
-    while (-1 != (opt = getopt_long(argc, argv, "dp:l", longopts, NULL))) {
+    while (-1 != (opt = getopt_long(argc, argv, "p:l", longopts, NULL))) {
         switch (opt) {
-            case 'D':
-                g_is_debug = 1;
-                break;
             case 'p':
                 g_port = atoi(optarg);
                 if (g_port < 1 || g_port > 65535) {
@@ -53,12 +49,54 @@ static int parse_options(int argc, char **argv)
 
 int main(int argc, char **argv)
 {
+    int server_sock, client_sock;
+    struct sockaddr_in server_addr, client_addr;
+
     if (argc > 1) {
         if (0 != parse_options(argc, argv)) {
             exit(-1);
         }
     }
-    printf("debug=%d\tport=%d\tlog=%d\n", g_is_debug, g_port, g_is_log);
+    // printf("port=%d\tlog=%d\n", g_port, g_is_log);
+
+    if ((server_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+        printf("Create socket failed\n");
+        return -1;
+    }
+
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(g_port);
+
+    if (bind(server_sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        printf("Bind server socket failed\n");
+        return -1;
+    }
+
+    if (listen(server_sock, 2) < 0) {
+        printf("Listen server socket failed\n");
+        return -1;
+    }
+
+    while(1) {
+        unsigned int client_len = sizeof(client_addr);
+
+        if ((client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_len)) < 0) {
+            printf("Accept client connection failed\n");
+            return -1;
+        }
+
+        char *headers = "Content-Type:text/html; charset=utf-8\r\n";
+
+        send(client_sock, headers, 2048, 0);
+        send(client_sock, "Hello Server\n", 2048, 0);
+
+        printf("Connection Success\n");
+
+
+        close(client_sock);
+    }
 
     return 0;
 }
--- server.h
+++ server.h
@@ -13,7 +13,6 @@
 #define DEFAULT_DOCUMENT_ROOT   "./"
 #define DEFAULT_LOG_PATH        "/tmp/mini_server.log"
 
-static unsigned short g_is_debug = 0;
 static unsigned int   g_port     = 8888;
 static unsigned short g_is_log   = 0;
 static char g_doc_root[256]      = DEFAULT_DOCUMENT_ROOT;
